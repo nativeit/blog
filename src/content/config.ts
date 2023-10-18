d2bd88fd465be668c491ffa554849889d4926ee6
Stephen Davis <sdavis@nativeit.net>
2023-10-17 23:41:22 -0400

    Added fonts

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,4 +12,19 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const gallery = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { 
+	'blog':  blog,
+	'gallery': gallery,
+};
